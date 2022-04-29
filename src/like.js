260452010f3e0bc0bf20908b7799106b6b3ec781
Amanda Manduca <amandavmanduca@hotmail.com>
2022-04-29 00:03:16 -0300

    :dizzy: Implements like animation

--- src/like.js
+++ src/like.js
@@ -1,14 +1,20 @@
 const likeButton = document.getElementById('like-icon-path');
+const likeIconClassList = document.getElementById("like-icon").classList
 
 likeButton.setAttribute("fill", "none")
+likeIconClassList.add('animate_like');
 
 function handleLike() {
     const activeColor = likeButton.getAttribute("fill")
     switch(activeColor) {
         case "red":
             likeButton.setAttribute("fill", "none")
+            likeButton.setAttribute("stroke", "black")
+            likeIconClassList.add('animate_like');
           break;
         default:
             likeButton.setAttribute("fill", "red")
+            likeButton.setAttribute("stroke", "red")
+            likeIconClassList.remove('animate_like')
       }
 }
