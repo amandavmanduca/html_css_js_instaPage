980904e7e29fb04bfe7c1dbf8f103199a74d1066
Amanda Manduca <amandavmanduca@hotmail.com>
2022-04-28 22:06:02 -0300

    :sparkles: Implements comments functionality

---
+++ src/comments.js
@@ -0,0 +1,28 @@
+const initialComment = 'consectetur adipiscing elit. Mi enim ut eu cras ultrices eget et'
+
+initialComment && addNewComment(initialComment)
+
+function addNewComment(user_text) {
+    const commentTitle = document.createElement('b');
+    commentTitle.className = 'comment-author'
+    commentTitle.innerHTML = 'Comment '
+    
+    const addNewComment = document.createElement('p');
+    addNewComment.className = 'comment-text'
+    var text = user_text;
+     
+    const commentsSection = document.getElementById('comments');
+    commentsSection.appendChild( addNewComment );
+    addNewComment.appendChild( commentTitle ).insertAdjacentText("afterend", text);
+    document.getElementById("comment-input").value = ''
+}
+
+const commentButton = document.getElementById('comment-button');
+commentButton.addEventListener("click", sendComment);
+
+function sendComment() {
+    const input_text = document.getElementById("comment-input").value
+    input_text && addNewComment(input_text)
+}
+ 
+
